2e0d589e2477a17d2c62443a966f66906e4adf8a
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-11 11:02:05 +0800

    Create Fenwick.cpp

---
+++ template/data structure/Fenwick.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Nehcoah on 2023/5/3.
+//
+
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+struct Fenwick {
+    int n;
+    vector<T> tree;
+
+    Fenwick(int n = 0) {
+        init(n);
+    }
+
+    void init(int sz) {
+        this->n = sz;
+        tree.assign(n, T());
+    }
+
+    void update(int pos, T val) {
+        for (; pos < tree.size(); pos += pos & -pos) tree[pos] += val;
+    }
+
+    T query(int pos) {
+        auto ans = T();
+        for (; pos; pos -= pos & -pos) ans += tree[pos];
+        return ans;
+    }
+
+    T query_interval(int l, int r) {
+        return query(r) - query(l - 1);
+    }
+
+};
