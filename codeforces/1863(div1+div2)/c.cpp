7367bde5faa54b48e9bcba6101ec9bf512894f80
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-13 13:51:45 +0800

    Update c.cpp

--- codeforces/1863(div1+div2)/c.cpp
+++ codeforces/1863(div1+div2)/c.cpp
@@ -10,35 +10,12 @@ void solve() {
     int n, k;
     cin >> n >> k;
     vector<int> a(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    if (k == 0) {
-        for (int i = 0; i < n; i++) cout << a[i] << " \n"[i == n - 1];
-        return;
-    }
-    set<int> st;
-    for (int i = 0; i <= n; i++) st.insert(i);
-    vector<int> f(n + 1);
-    for (int i = 0; i < n; i++) {
-        if (st.count(a[i])) st.erase(st.find(a[i]));
-        f[a[i]]++;
-    }
-    for (int i = 0; i < n; i++) {
-        int x = a[i];
-        f[x]--;
-        a[i] = *st.begin();
-        st.erase(st.begin());
-        if (f[x] == 0) st.insert(x);
-    }
-
-    k--;
-    vector<bool> vis(n + 1);
-    int cur;
-    for (int i = 0; i < n; i++) vis[a[i]] = 1;
-    for (int i = 0; i < n + 1; i++) if (!vis[i]) cur = i;
-    int diff = k % (n + 1);
-    a.push_back(cur);
-    for (int i = diff == 0 ? 0 : n + 1 - diff, cnt = 0; cnt < n; cnt++, i = (i + 1) % (n + 1)) cout << a[i] << " ";
-    cout << endl;
+    long long b = (long long) n * (n + 1) / 2;
+    for (int i = 0; i < n; i++) cin >> a[i], b -= a[i];
+    a.push_back(b);
+    k %= (n + 1);
+    rotate(a.begin(), a.end() - k, a.end());
+    for (int i = 0; i < n; i++) cout << a[i] << " \n"[i == n - 1];
 }
 
 int main() {
