bfb13356213f37f992945319f702e3c59fb541cd
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-13 13:54:02 +0800

    Create a.cpp

---
+++ codeforces/1861(div2)/a.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+
+#define endl '\n'
+using namespace std;
+using ll = long long;
+const int mod = 998244353;
+const int inf = 0x3f3f3f3f;
+
+void solve() {
+    string s;
+    cin >> s;
+    unordered_set<int> st{11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+    int cur = int(s[0] - '0') * 10;
+    for (int i = 1; i < 9; i++) {
+        int c = cur + int(s[i] - '0');
+        if (st.count(c)) {
+            cout << c << endl;
+            return;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
