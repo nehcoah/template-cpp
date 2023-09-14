97ec97e1f8c478ada972fb1c8139cdbcbdbd1f01
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-14 11:04:06 +0800

    Create b.cpp

---
+++ leetcode/weekly/357/b.cpp
@@ -0,0 +1,15 @@
+// 2811
+// https://leetcode.cn/problems/check-if-it-is-possible-to-split-array/
+// 
+
+class Solution {
+public:
+    bool canSplitArray(vector<int>& nums, int m) {
+        int n = nums.size();
+        if (n < 3) return true;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] + nums[i + 1] >= m) return true;
+        }
+        return false;
+    }
+};
