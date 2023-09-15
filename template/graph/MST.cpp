f0b5d02fc0a1507a4a118623af3c3d34adb53afc
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-15 10:38:51 +0800

    Update MST.cpp

--- template/graph/MST.cpp
+++ template/graph/MST.cpp
@@ -9,7 +9,10 @@
 
 using namespace std;
 
+// kruskal algorithm
 // edges[i] = [start, end, weight]
+// n = edges.size()
+// time: O(nlog(n)), depends on edges
 int kruskal(int n, vector<vector<int>>& edges) {
     int cnt = n;
     vector<int> parent(n);
@@ -35,4 +38,34 @@ int kruskal(int n, vector<vector<int>>& edges) {
         if (merge(e[0], e[1])) v += e[2];
     }
     return v;
+}
+
+// prim algorithm
+// g = Adjacency Matrix
+// n = adj.size()
+// time: O(n^2), depend on points
+int prim(vector<vector<int>> &g, int start) {
+    int ans = 0;
+    int n = g.size();
+    vector<int> lowcost(n, INT_MAX);
+    vector<int> vis(n);
+
+    lowcost[start] = 0;
+    for (int i = 0; i < n; i++) {
+        int minIdx = -1;
+        int minVal = INT_MAX;
+        for (int j = 0; j < n; j++) {
+            if (vis[j] == 0 && lowcost[j] <= minVal) {
+                minIdx = j;
+                minVal = lowcost[j];
+            }
+        }
+        vis[minIdx] = 1;
+        ans += minVal;
+
+        for (int j = 0; j < n; j++) {
+            if (vis[j] == 0) lowcost[j] = min(lowcost[j], g[j][minIdx]);
+        }
+    }
+    return ans;
 }
