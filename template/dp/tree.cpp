a6abb80fdf8de84a036416f16b81269e4bd8bcb9
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-17 17:04:07 +0800

    Update tree.cpp

--- template/dp/tree.cpp
+++ template/dp/tree.cpp
@@ -1,7 +1,7 @@
 // 换根dp
-// Leetcode. 834. https://leetcode.cn/problems/sum-of-distances-in-tree/description/
 // 两次遍历
 
+// Leetcode. 834. https://leetcode.cn/problems/sum-of-distances-in-tree/description/
 class Solution {
 public:
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
@@ -26,6 +26,40 @@ public:
             for (int y : adj[x]) {
                 if (y == pre) continue;
                 ans[y] = ans[x] - (size[y] - 1) + (n - size[y] - 1);
+                self(self, y, x);
+            }
+        };
+        reroot(reroot, 0, -1);
+
+        return ans;
+    }
+};
+
+
+// https://leetcode.cn/problems/minimum-edge-reversals-so-every-node-is-reachable/description/
+class Solution {
+public:
+    vector<int> minEdgeReversals(int n, vector<vector<int>>& edges) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto &e : edges) {
+            adj[e[0]].emplace_back(e[1], 1);
+            adj[e[1]].emplace_back(e[0], -1);
+        }
+
+        vector<int> ans(n);
+        auto dfs = [&](auto self, int x, int pre) -> void {
+            for (auto [y, d] : adj[x]) {
+                if (y == pre) continue;
+                if (d == -1) ans[0] += 1;
+                self(self, y, x);
+            }
+        };
+        dfs(dfs, 0, -1);
+
+        auto reroot = [&](auto self, int x, int pre) -> void {
+            for (auto [y, d] : adj[x]) {
+                if (y == pre) continue;
+                ans[y] = ans[x] + d;
                 self(self, y, x);
             }
         };
