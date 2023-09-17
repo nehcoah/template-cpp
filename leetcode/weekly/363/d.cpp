40debe8e8866c2f92b83f2923fb160c47428e78c
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-17 12:11:12 +0800

    Create d.cpp

---
+++ leetcode/weekly/363/d.cpp
@@ -0,0 +1,17 @@
+// 
+
+class Solution {
+public:
+    long long maximumSum(vector<int>& nums) {
+        long long ans = 0;
+        int n = nums.size();
+        for (int i = 1; i <= n; i++) {
+            long long cur = 0;
+            for (int j = i; j * j / i <= n; j += i) {
+                cur += nums[j * j / i - 1];
+            }
+            ans = max(ans, cur);
+        }
+        return ans;
+    }
+};
