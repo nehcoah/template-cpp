607036ca163a59ca0847b5b76a88e44c00b63e93
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-17 15:05:56 +0800

    Update d.cpp

--- leetcode/weekly/363/d.cpp
+++ leetcode/weekly/363/d.cpp
@@ -1,5 +1,9 @@
 // https://leetcode.cn/problems/maximum-element-sum-of-a-complete-subset-of-indices/
 
+// 1, 4, 9, 16, 25, ...
+// 2*1, 2*4, 2*9, 2*16, 2*25, ...
+// 3*1, 3*4, 3*9, 3*16, 3*25, ...
+
 class Solution {
 public:
     long long maximumSum(vector<int>& nums) {
@@ -7,8 +11,8 @@ public:
         int n = nums.size();
         for (int i = 1; i <= n; i++) {
             long long cur = 0;
-            for (int j = i; j * j / i <= n; j += i) {
-                cur += nums[j * j / i - 1];
+            for (int j = 1; i * j * j <= n; j++) {
+                cur += nums[i * j * j - 1];
             }
             ans = max(ans, cur);
         }
