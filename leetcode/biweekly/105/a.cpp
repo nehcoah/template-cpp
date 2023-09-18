5553c8d79a53ee9e003f40345baa6c51e831abba
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-18 11:49:27 +0800

    Create a.cpp

---
+++ leetcode/biweekly/105/a.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.cn/problems/buy-two-chocolates/
+
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        int x = money - prices[0] - prices[1];
+        return x >= 0 ? x : money;
+    }
+};
