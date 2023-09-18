cd6b97b71f1470862658611629a64f5086ad9cee
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-18 14:56:41 +0800

    Create a.py

---
+++ leetcode/biweekly/104/a.py
@@ -0,0 +1,9 @@
+# https://leetcode.cn/problems/number-of-senior-citizens/
+
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        ans = 0
+        for s in details:
+            if int(s[-4:-2]) > 60:
+                ans += 1
+        return ans
