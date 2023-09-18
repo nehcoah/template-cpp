083188ec94b2387d30005f8cc32355cd9db332e8
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-18 11:12:51 +0800

    Create a.cpp

---
+++ leetcode/weekly/354/a.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.cn/problems/sum-of-squares-of-special-elements/
+
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int n = nums.size(), ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (n % (i + 1) == 0) ans += nums[i] * nums[i];
+        }
+        return ans;
+    }
+};
