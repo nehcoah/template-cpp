784bc20a9a3505fb5260639be1672810a91f337c
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-21 14:28:21 +0800

    Update b.cpp

--- leetcode/weekly/343/b.cpp
+++ leetcode/weekly/343/b.cpp
@@ -1,2 +1,24 @@
 // https://leetcode.cn/problems/first-completely-painted-row-or-column/
 
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<pair<int, int>> idx(m * n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                idx[mat[i][j] - 1] = {i, j};
+            }
+        }
+        int row[m], col[n];
+        memset(row, 0, sizeof(row));
+        memset(col, 0, sizeof(col));
+        for (int i = 0; i < arr.size(); i++) {
+            auto [x, y] = idx[arr[i] - 1];
+            row[x]++;
+            col[y]++;
+            if (row[x] == n || col[y] == m) return i;
+        }
+        return -1;
+    }
+};
