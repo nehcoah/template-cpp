2627c30eefc67cd48db360ced31521644a0c4278
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-22 10:33:10 +0800

    Update c.cpp

--- leetcode/weekly/341/c.cpp
+++ leetcode/weekly/341/c.cpp
@@ -3,35 +3,45 @@
 class Solution {
 public:
     int addMinimum(string s) {
-        int n = s.length();
-        int ans = 0;
-        for (int i = 0; i < n;){
-            if (s[i] == 'a'){
-                if (i == n - 1){
-                    ans += 2;
-                    break;
-                } else if(s[i + 1] == 'a') {
-                    ans += 2;
-                    i += 1;
-                } else if (i < n - 2 && s[i + 1] == 'b' && s[i + 2] == 'c') i += 3;
-                else if (s[i + 1] == 'b' || s[i + 1] == 'c') {
-                    ans += 1;
-                    i += 2;
-                }
-            } else if (s[i] == 'b'){
-                ans += 1; //a
-                if (i == n - 1){
-                    ans += 1;
-                    break;
-                } else if(s[i + 1] != 'c'){
-                    ans += 1;
-                    i += 1;
-                } else i += 2;
-            } else {
-                ans += 2;
-                i += 1;
-            }
-        }
+        int ans = s[0] + 2 - s.back();
+        for (int i = 1; i < s.length(); ++i)
+            ans += (s[i] + 2 - s[i - 1]) % 3;
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int addMinimum(string s) {
+//         int n = s.length();
+//         int ans = 0;
+//         for (int i = 0; i < n;){
+//             if (s[i] == 'a'){
+//                 if (i == n - 1){
+//                     ans += 2;
+//                     break;
+//                 } else if(s[i + 1] == 'a') {
+//                     ans += 2;
+//                     i += 1;
+//                 } else if (i < n - 2 && s[i + 1] == 'b' && s[i + 2] == 'c') i += 3;
+//                 else if (s[i + 1] == 'b' || s[i + 1] == 'c') {
+//                     ans += 1;
+//                     i += 2;
+//                 }
+//             } else if (s[i] == 'b'){
+//                 ans += 1; //a
+//                 if (i == n - 1){
+//                     ans += 1;
+//                     break;
+//                 } else if(s[i + 1] != 'c'){
+//                     ans += 1;
+//                     i += 1;
+//                 } else i += 2;
+//             } else {
+//                 ans += 2;
+//                 i += 1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
