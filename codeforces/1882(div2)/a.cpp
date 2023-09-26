7a67b9de870f03886966c0651a50941ce3d084af
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-09-26 12:02:25 +0800

    Create a.cpp

---
+++ codeforces/1882(div2)/a.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    int ans = 1;
+    for (int x : a) {
+        if (ans != x) ans++;
+        else ans = x + 2;
+    }
+    cout << ans - 1 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
