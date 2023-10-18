2f03bcd2c1ca63eebfdcca3d7166410ef9f50af2
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-10-18 17:40:33 +0800

    Update Sieve.cpp

--- template/others/Sieve.cpp
+++ template/others/Sieve.cpp
@@ -1,18 +1,18 @@
-vector<int> is_prime, minp, prime;
+vector<int> is_prime, mnp, prime;
 void sieve(int n) {
     is_prime.assign(n + 1, true);
-    minp.assign(n + 1, 0);
+    mnp.assign(n + 1, 0);
     is_prime[0] = is_prime[1] = false;
     for (int i = 2; i <= n; i++) {
         if (is_prime[i]) {
             prime.push_back(i);
-            minp[i] = i;
+            mnp[i] = i;
         }
         for (auto x : prime) {
             if (i * x > n) break;
             is_prime[i * x] = false;
-            minp[i * x] = x;
-            if (x == minp[i]) break;
+            mnp[i * x] = x;
+            if (x == mnp[i]) break;
         }
     }
 }
