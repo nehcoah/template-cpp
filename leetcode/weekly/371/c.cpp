ce4e488c88fef96a673a89ef306e6ab2927f3d57
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-11-12 12:55:35 +0800

    Update weekly 371 c.cpp

--- leetcode/weekly/371/c.cpp
+++ leetcode/weekly/371/c.cpp
@@ -3,23 +3,20 @@ public:
     int minOperations(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         
-        auto check = [&]() {
+        auto check = [&]() -> int {
             int cur = 0;
             for (int i = 0; i < n - 1; i++) {
                 if (nums1[i] > nums1.back() || nums2[i] > nums2.back()) {
                     if (nums1[i] <= nums2.back() && nums2[i] <= nums1.back()) cur++;
-                    else return -2;
+                    else return 1e5;
                 }
             }
             return cur;
         };
         
-        int ans1 = check();
+        int ans = check();
         swap(nums1.back(), nums2.back());
-        int ans2 = check() + 1;
-        if (ans1 < 0 && ans2 < 0) return -1;
-        else if (ans1 < 0) return ans2;
-        else if (ans2 < 0) return ans1;
-        else return min(ans1, ans2);
+        ans = min(ans, check() + 1);
+        return ans > n ? -1 : ans;
     }
 };
