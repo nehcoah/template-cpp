65fe272563047b9792e7cc39d89da101ee22d472
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-11-19 15:41:54 +0800

    Update SparseTable.cpp

--- template/data_structure/SparseTable.cpp
+++ template/data_structure/SparseTable.cpp
@@ -1,24 +1,25 @@
 template<typename Info>
 class SparseTable {
+    int n;
     vector<vector<Info>> st;
 public:
     SparseTable(const vector<Info> &v) {
-        int n = v.size(), l1 = ceil(log2(n)) + 1;
-        st.assign(n, vector<Info>(l1, Info()));
-        for (int i = 0; i < n; i++) {
-            st[i][0] = v[i];
-        }
-        for (int j = 1; j < l1; j++) {
-            int pj = 1 << (j - 1);
-            for (int i = 0; i + pj < n; i++) {
-                st[i][j] = st[i][j - 1] + st[i + (1 << (j - 1))][j - 1];
+        n = v.size();
+        if (n == 0) return;
+        int maxLog = 32 - __builtin_clz(n);
+        st.resize(maxLog);
+        st[0] = v;
+        for (int j = 1; j < maxLog; j++) {
+            st[j].resize(n - (1 << j) + 1);
+            for (int i = 0; i <= n - (1 << j); i++) {
+                st[j][i] = st[j - 1][i] + st[j - 1][i + (1 << (j - 1))];
             }
         }
     }
 
     Info query(int l, int r) {
-        int lt = r - l + 1;
-        int q = floor(log2(lt));
-        return st[l][q] + st[r - (1 << q) + 1][q];
+        assert(0 <= l && l <= r && r <= n - 1);
+        int lg = 31 - __builtin_clz(r - l + 1);
+        return st[lg][l] + st[lg][r - (1 << lg) + 1];
     }
 };
