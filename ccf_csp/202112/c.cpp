39e14c4743adb9f247c03bdb263249a00f288998
nehcoah <120350642+nehcoah@users.noreply.github.com>
2023-12-08 15:12:54 +0800

    Update c.cpp

--- ccf_csp/202112/c.cpp
+++ ccf_csp/202112/c.cpp
@@ -1,5 +1,3 @@
-// 40
-
 #include "bits/stdc++.h"
 
 using namespace std;
@@ -9,45 +7,70 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    // 0 -> Upper, 1 -> Lower, 2 -> digit
+    int mode = 0;
+    vector<int> info;
+
     int w, s;
-    cin >> w >> s;
-    string str;
-    cin >> str;
-    int f = 0;
-    vector<int> ans;
-    for (char c : str) {
+    string t;
+    cin >> w >> s >> t;
+    for (char c : t) {
         if (isdigit(c)) {
-            if (f != 2) {
-                ans.push_back(28);
-                f = 2;
-            }
-            ans.push_back(c - '0');
+            if (mode != 2) info.push_back(28);
+            info.push_back(c - '0');
+            mode = 2;
         } else if (islower(c)) {
-            if (f != 1) {
-                ans.push_back(27);
-                f = 1;
-            }
-            ans.push_back(c - 'a');
+            if (mode != 1) info.push_back(27);
+            info.push_back(c - 'a');
+            mode = 1;
         } else {
-            if (f != 0) {
-                if (f == 1) ans.push_back(28);
-                ans.push_back(28);
-                f = 0;
+            if (mode != 0) {
+                if (mode == 1) info.push_back(28);
+                info.push_back(28);
             }
-            ans.push_back(c - 'A');
+            info.push_back(c - 'A');
+            mode = 0;
         }
     }
-    if (ans.size() % 2 == 1) ans.push_back(29);
-    vector<int> tmp(ans.size() / 2);
-    for (int i = 0; i < ans.size(); i += 2) {
-        tmp[i / 2] = ans[i] * 30 + ans[i + 1];
+    if (info.size() % 2 == 1) info.push_back(29);
+
+    vector<int> d(1), g{1, 926};
+    int k = s == -1 ? 0 : pow(2, s + 1), cur = 926, mod = 929;
+    for (int i = 0; i < info.size(); i += 2) {
+        d.push_back(info[i] * 30 + info[i + 1]);
     }
-    ans.swap(tmp);
-    while ((ans.size() + 1) % w != 0) ans.push_back(900);
-    cout << ans.size() + 1 << "\n";
+    int append = (int(d.size()) + k) % w;
+    if (append > 0) append = w - append;
+    for (int i = 0; i < append; i++) {
+        d.push_back(900);
+    }
+    d[0] = d.size();
+    
+    auto ans = d;
+    if (s != -1) {
+        for (int i = 0; i < k; i++) d.push_back(0);
+        for (int i = 1; i < k; i++) {
+            cur = cur * 3 % mod;
+            vector<int> c{1, cur}, curg(g.size() + 1);
+            for (int x = 0; x < g.size(); x++) {
+                for (int y = 0; y < 2; y++) {
+                    curg[x + y] = (curg[x + y] + g[x] * c[y] % mod) % mod;
+                }
+            }
+            g.swap(curg);
+        }
+        for (int i = 0; i <= d.size() - g.size(); i++) {
+            int x = d[i]; d[i] = 0;
+            for (int j = 1; j < g.size(); j++) {
+                d[i + j] = ((d[i + j] - g[j] * x) % mod + mod) % mod;
+            }
+        }
+        for (int i = d.size() - g.size() + 1; i < d.size(); i++) {
+            ans.push_back((-d[i] + mod) % mod);
+        }
+    }
+
     for (int i = 0; i < ans.size(); i++) cout << ans[i] << "\n";
 
     return 0;
 }
-
-
