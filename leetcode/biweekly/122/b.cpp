73f5ad8617188779f543668475798f40ff0d7292
nehcoah <120350642+nehcoah@users.noreply.github.com>
2024-01-21 13:42:56 +0800

    Update biweekly 112 b.cpp

--- leetcode/biweekly/122/b.cpp
+++ leetcode/biweekly/122/b.cpp
@@ -2,19 +2,37 @@ class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> a(n), idx(n);
+        vector<int> a(n);
         for (int i = 0; i < n; i++) {
             a[i] = __builtin_popcount(nums[i]);
         }
-        iota(idx.begin(), idx.end(), 0);
-        ranges::sort(idx, [&](int i, int j) {return nums[i] < nums[j];});
-        for (int i = 0; i < n; i++) {
-            int t = -1;
-            for (int x = min(i, idx[i]); x <= max(i, idx[i]); x++) {
-                if (t == -1) t = a[x];
-                else if (t != a[x]) return false;
-            }
+        for (int i = 0; i < n; ) {
+            int j = i, x = a[i];
+            while (j < n && a[j] == x) j++;
+            sort(nums.begin() + i, nums.begin() + j);
+            i = j;
         }
-        return true;
+        return is_sorted(nums.begin(), nums.end());
     }
 };
+
+// class Solution {
+// public:
+//     bool canSortArray(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> a(n), idx(n);
+//         for (int i = 0; i < n; i++) {
+//             a[i] = __builtin_popcount(nums[i]);
+//         }
+//         iota(idx.begin(), idx.end(), 0);
+//         ranges::sort(idx, [&](int i, int j) {return nums[i] < nums[j];});
+//         for (int i = 0; i < n; i++) {
+//             int t = -1;
+//             for (int x = min(i, idx[i]); x <= max(i, idx[i]); x++) {
+//                 if (t == -1) t = a[x];
+//                 else if (t != a[x]) return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
