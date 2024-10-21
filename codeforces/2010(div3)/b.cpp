51e7ea8973ae9aac454494823238a63a0722d248
nehcoah <haochenlyu@gmail.com>
2024-10-21 10:49:04 +0800

    update cf2010(div3)

---
+++ codeforces/2010(div3)/b.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using u32 = unsigned;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b;
+    cin >> a >> b;
+    cout << 6 - a - b << endl;
+
+    return 0;
+}
+
